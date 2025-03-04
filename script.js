753bd05e4b4bc6a9950cfdffb20f849508a91ef3
PrivacySecured <LoveNewfies@proton.me>
2025-03-04 16:27:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// On Website Load
 window.onloadTurnstileCallback = function () {
     turnstile.render("#captchaContainer", {
-      sitekey: "0x4AAAAAAA_jkIM42Rp4MiQU",
-      callback: function (token) {
-        const captcha = document.getElementById('captchaContainer');
-        captcha.style.display = "none";
-      },
+        sitekey: "0x4AAAAAAA_jkIM42Rp4MiQU",
+        callback: function (token) {
+            const captcha = document.getElementById('captchaContainer');
+            captcha.style.display = "none";
+        },
     });
-  };
+};
+
+// Before You're Allowed To Click The Button
