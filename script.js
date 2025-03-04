fca4a7a18562c51bb3522ab97a5d679ec21feff7
PrivacySecured <LoveNewfies@proton.me>
2025-03-04 16:23:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ window.onloadTurnstileCallback = function () {
     turnstile.render("#captchaContainer", {
       sitekey: "0x4AAAAAAA_jkIM42Rp4MiQU",
       callback: function (token) {
-        console.log(`Challenge Success ${token}`);
+        const captcha = document.getElementById('captchaContainer');
+        captcha.style.display = "none";
       },
     });
   };
